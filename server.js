c3cd792677e4131b4c9ed44dd5b2d1ddbda273eb
Alina <mlnvccc@gmail.com>
2021-06-03 16:21:12 +0300

    index router

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { middleware } = require('./src/middleware/middleware');
 require('dotenv').config();
 
 // Импорт ручек
+const indexRouter = require ('./src/routes/indexRouter')
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,6 +15,7 @@ middleware(app);
 
 // Подключение ручек
 
+app.use('/',indexRouter)
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
