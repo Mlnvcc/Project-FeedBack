1146536ecd78ea424da430edf95aabbc6d72d831
Roman <makatsariya_roma@mail.ru>
2021-06-04 10:56:15 +0300

    view feedBacks to profile

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const { protectProfile } = require('../middleware/middleware');
+const FeedBack = require('../models/feedbackModel');
 
 const router = Router();
 
 router.route('/')
-  .get(protectProfile, (req, res) => {
-    res.render('profile');
+  .get(protectProfile, async (req, res) => {
+    const userFeedBacks = await FeedBack.find({ author: req.session.userId }).populate('positionName').populate('technologies');
+    console.log(userFeedBacks);
+    res.render('profile', { userFeedBacks });
   });
 
 module.exports = router;
